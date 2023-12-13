cad1caa946249143a5e49129f24399b11415da43
Malte Ollmann <60318279+Duderinooo@users.noreply.github.com>
2023-12-13 11:08:40 +0100

    removed ""

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -5,8 +5,8 @@ export const Hero = () => {
     <div>
       <div className="absolute top-[50vh] z-10 w-screen text-center text-white">
         <p className="text-2xl" style={{ fontFamily: 'cursive' }}>
-          "Friede beginnt damit, daß jeder von uns sich jeden Tag um seinen
-          Körper und seinen Geist kümmert."
+          Friede beginnt damit, daß jeder von uns sich jeden Tag um seinen
+          Körper und seinen Geist kümmert.
         </p>
         <p className="mt-4 text-xl" style={{ fontFamily: 'cursive' }}>
           Thich Nhat Hanh
