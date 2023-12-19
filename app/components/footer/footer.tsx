c55fe4f360d107f58fe4374bdf51c8328838e44c
Malte Ollmann <60318279+Duderinooo@users.noreply.github.com>
2023-12-19 16:20:21 +0100

    feat(mobile): Added further mobile and breakpoint improvements

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,30 +1,37 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export const Footer = () => {
   return (
-    <div className="flex min-h-[300px] w-screen flex-col items-center justify-center justify-between gap-8 bg-gradient-to-r from-tertiary-400 to-primary-400 px-4 py-8 text-center text-white sm:px-10 sm:text-left md:flex md:flex-row md:p-14 lg:px-28 lg:py-24 xl:px-60 ">
-      <div className="flex flex-col gap-4 md:flex-row md:gap-24">
-        <div>
-          <p className="mb-2 text-3xl">Kontakt</p>
-          <p>Tina Ollmann</p>
-          <p>Möhrenweg 2</p>
-          <p>22334 Möhrenhausen</p>
-          <p>Telefon: 0120120120120</p>
-          <p>Email: lichtOrt@gmail.de</p>
+    <div className="min-h-[300px] w-screen bg-gradient-to-r from-tertiary-400 to-primary-400 px-4 py-8 text-white sm:px-10 md:p-14 lg:px-28 lg:py-24 xl:px-60 ">
+      <div className="flex flex-col items-center justify-between gap-8 text-center sm:text-left md:flex md:flex-row ">
+        <div className="flex flex-col gap-4 md:flex-row md:gap-24">
+          <div>
+            <p className="mb-2 text-3xl">Kontakt</p>
+            <p>Tina Ollmann</p>
+            <p>Möhrenweg 2</p>
+            <p>22334 Möhrenhausen</p>
+            <p>Telefon: 0120120120120</p>
+            <p>Email: lichtOrt@gmail.de</p>
+          </div>
+          <div>
+            <p className="mb-2 text-3xl">Rechtliches</p>
+            <Link className="block" href={'/datenschutz#datenschutz'}>
+              Datenschutz
+            </Link>
+            <Link className="block" href={'/impressum#impressum'}>
+              Impressum
+            </Link>
+          </div>
         </div>
-        <div>
-          <p className="mb-2 text-3xl">Rechtliches</p>
-          <p>Adresse</p>
-          <p>Telefon</p>
-          <p>Email</p>
-        </div>
+        <Image
+          src={'/images/logo_clear.svg'}
+          alt={'LichtOrt company logo: Yoga, Meditation, Healing'}
+          height={128}
+          width={128}
+        />
       </div>
-      <Image
-        src={'/images/logo_clear.svg'}
-        alt={'LichtOrt company logo: Yoga, Meditation, Healing'}
-        height={128}
-        width={128}
-      />
+      <div className="mt-10">©2023 LichtOrt</div>
     </div>
   );
 };
--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -15,38 +15,38 @@ export const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <div className="sticky top-[-90vh] z-10 h-screen w-screen">
+    <div className="sticky top-0 z-10 grid h-[9vh] w-screen place-content-center sm:top-[-41vh] sm:block sm:h-[50vh] md:top-[-89vh] md:h-screen">
       <Hero />
-      <div className="absolute bottom-0 w-screen p-4 text-center text-white">
-        <div className="flex justify-center gap-4">
+      <div className="relative bottom-0 my-auto w-screen py-[6px] text-center text-white sm:absolute sm:p-4">
+        <div className="flex justify-around gap-4 sm:justify-center sm:gap-10 md:gap-4">
           <Link
             key={'Startseite'}
-            href={'/'}
+            href={'/#start'}
             className={clsx(
-              'flex gap-2 p-3 text-2xl font-medium hover:text-primary-400 md:flex-none md:justify-start md:p-2 md:px-3',
+              'flex gap-2 p-3 text-xl font-medium hover:text-primary-400 md:flex-none md:justify-start md:p-2 md:px-3 lg:text-2xl',
               {
                 'text-primary-400': pathname === '/',
               },
             )}
           >
-            <HomeIcon className="w-6" />
-            <p className="m-auto">Startseite</p>
+            <HomeIcon className="w-6 md:w-8 " />
+            <p className="m-auto hidden md:block">Startseite</p>
           </Link>
           <Link
             key={'Termine'}
             href={'/termine'}
             className={clsx(
-              'flex gap-2 p-3 text-2xl font-medium hover:text-primary-400 md:flex-none md:justify-start md:p-2 md:px-3',
+              'flex gap-2 p-3 text-xl font-medium hover:text-primary-400 md:flex-none md:justify-start md:p-2 md:px-3 lg:text-2xl',
               {
                 'text-primary-400': pathname === '/termine',
               },
             )}
           >
-            <CalendarIcon className="w-6" />
-            <p className="m-auto">Termine</p>
+            <CalendarIcon className="w-6 md:w-8 " />
+            <p className="m-auto hidden md:block">Termine</p>
           </Link>
           <Image
-            className="ml-[-18px]"
+            className="mx-4 my-auto hidden h-10 w-10 sm:block md:h-16 md:w-16"
             src={'/images/logo_clear.svg'}
             alt={'LichtOrt company logo: Yoga, Meditation, Healing'}
             height={64}
@@ -55,29 +55,29 @@ export const Navbar = () => {
           />
           <Link
             key={'Angebot'}
-            href={'/angebot'}
+            href={'/angebot#start'}
             className={clsx(
-              'flex gap-2 p-3 text-2xl font-medium hover:text-primary-400 md:flex-none md:justify-start md:p-2 md:px-3',
+              'flex gap-2 p-3 text-xl font-medium hover:text-primary-400 md:flex-none md:justify-start md:p-2 md:px-3 lg:text-2xl',
               {
                 'text-primary-400': pathname === '/angebot',
               },
             )}
           >
-            <BookOpenIcon className="w-6" />
-            <p className="m-auto">Angebot</p>
+            <BookOpenIcon className="w-6 md:w-8 " />
+            <p className="m-auto hidden md:block">Angebot</p>
           </Link>
           <Link
             key={'Mediathek'}
             href={'/media'}
             className={clsx(
-              'flex gap-2 p-3 text-2xl font-medium hover:text-primary-400 md:flex-none md:justify-start md:p-2 md:px-3',
+              'flex gap-2 p-3 text-xl font-medium hover:text-primary-400 md:flex-none md:justify-start md:p-2 md:px-3 lg:text-2xl',
               {
                 'text-primary-400': pathname === '/media',
               },
             )}
           >
-            <PhotoIcon className="w-6" />
-            <p className="m-auto">Mediathek</p>
+            <PhotoIcon className="w-6 md:w-8 " />
+            <p className="m-auto hidden md:block">Mediathek</p>
           </Link>
         </div>
       </div>
